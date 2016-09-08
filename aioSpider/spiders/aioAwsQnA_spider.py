f4d22ef7eb3afaf09baf847fc0fcc06bfe015d10
Mystique <b4wt@quantumfoam.uni.cc>
2016-09-09 00:48:24 +0530

    Questions, Answers, Choices, Comments all in JSON working
    Signed-off-by: Mystique <b4wt@quantumfoam.uni.cc>

--- aioSpider/spiders/aioAwsQnA_spider.py
+++ aioSpider/spiders/aioAwsQnA_spider.py
@@ -2,40 +2,53 @@ import scrapy
 
 class aioAwsQnAItem(scrapy.Item):
     question = scrapy.Field()
-    answerChoices = scrapy.Field()
-    correctChoices = scrapy.Field()
+    options = scrapy.Field()
+    answers = scrapy.Field()
+    comments = scrapy.Field()
 
 class aioAwsQnASpider (scrapy.Spider):
     name = "aioAwsQnASpider"
     allowed_domains = ["aiotestking.com"]
-    start_urls = [
-        "http://www.aiotestking.com/amazon/which-of-the-following-options-would-enable-an-equivalent-experience-for-users-on-both-continents/",
-        "http://www.aiotestking.com/amazon/which-methods-will-enable-the-branch-office-to-access-their-data/",
-        "http://www.aiotestking.com/amazon/which-two-methods-increases-the-fault-tolerance-of-the-connection-to-vpc-1/"
-    ]
+    
+    # Pass Scrapy Spider a list of URLs to crawl via .txt file
+    # http://stackoverflow.com/questions/17307718/pass-scrapy-spider-a-list-of-urls-to-crawl-via-txt-file
+    # Pass through strip to remove any trailing newline characters
+    def __init__(self, filename=None):
+        if filename:
+            with open(filename, 'r') as f:
+                self.start_urls = [url.strip() for url in f.readlines()]
 
     def parse(self, response):
-        #qaData = response.css("#content p").xpath('.//text()').extract()
+        
+        # QUESTION - Scraping
         qaData = response.css("#content > p")
         qItems = []
         qItems = aioAwsQnAItem()
         
-        tempList=[]
-        tempList=qaData[0].css('::text').extract()
+        tempList = []
+        tempList = qaData[0].css('::text').extract()
         qItems['question'] = " ".join(tempList)
-        
+
+        # CHOICES & ANSWERS - Scraping        
         # Create a two pair(answerID & text) from the remaining to get the answers text
         # Run a join to ensure any long answer text is not broken and unnecessary spaces are removed
-        answerText=[]
-        for i in range(1,len(qaData),1):
-            qaData[i]=qaData[i].css("::text").extract()
-            answerText.append("".join(qaData[i]))
-       
-        # Assing to scrapy Item
-        qItems['answerChoices'] = answerText
-        
-        qItems['correctChoices']=response.css("#content > p").re(r'<p>(.*).<br><font')
-        print '\t\t---------BEGIN POST-----'
-        print qItems
+        qItems['answers'] = qaData.re(r'<p>(.*).<br><font')		
+        tempChoices = qaData
+        tempList = []
+        for i in range(1,len(tempChoices),1):
+            tempChoices[i] = tempChoices[i].css("::text").extract()
+            tempList.append("".join(tempChoices[i]))
+        qItems['options'] = tempList
+
+        # COMMENTS - Scraping
+        tempList = []
+        for sel in response.css("#commentlist > li"):
+            qComments = sel.css("p::text").extract()
+            qCommentAuthor = sel.css("cite::text").extract()
+            print qCommentAuthor, qComments
+            tempList.append([qCommentAuthor, qComments])
+        qItems['comments'] = tempList        
+
+        # Return the JSON to be writtent to file
         yield qItems
-        print '\t\t---------END POST------- \n\n\n\n'
+        
